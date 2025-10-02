40d45e4072734b6189a9a2e33f8606fdf9dceeb7
Ellie <melehulakaka@gmail.com>
2025-10-02 11:23:08 +0200

    Fixed loot table not working correctly

--- src/main/java/net/elysieon/aetherpunk/util/LootTableModifier.java
+++ src/main/java/net/elysieon/aetherpunk/util/LootTableModifier.java
@@ -3,25 +3,31 @@ package net.elysieon.aetherpunk.util;
 import net.elysieon.aetherpunk.index.AetherpunkItems;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LootTableModifier {
 
+    // Trail Ruin Rare Archaeology Modifier :3
     public static void LootTableModifier() {
-        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) -> {
             if(LootTables.TRAIL_RUINS_RARE_ARCHAEOLOGY.equals(id)) {
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.10f))
-                        .with(ItemEntry.builder(AetherpunkItems.ANCIENT_CORE))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
-                tableBuilder.pool(poolBuilder.build());
-            }
+                     List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
+                     entries.add(ItemEntry.builder(AetherpunkItems.ANCIENT_CORE).build());
+                     LootPool.Builder pool = LootPool.builder().with(entries);
+                     return LootTable.builder().pool(pool).build();
+                 }
+            return null;
         });
     }
 }
